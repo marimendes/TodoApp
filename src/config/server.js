66e13974aa94a800dc2a77592bd303c1cced6bcf
Mariana Mendes <mare.s.mendes@gmail.com>
2018-07-26 14:01:18 -0300

    Mapeando rotas

--- src/config/server.js
+++ src/config/server.js
@@ -8,4 +8,6 @@ server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.listen(port, function() {
 	console.log(`BACKEND is running on port ${port}.`)
-}) 
+})
+
+module.exports = server
